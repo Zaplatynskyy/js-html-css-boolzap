d820273063a75a32d2104873af08fca4e48b9758
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-02 17:36:03 +0100

    settings al click

--- js/script.js
+++ js/script.js
@@ -9,20 +9,18 @@ const app = new Vue({
                 messages: [{
                     date: '10/01/2020 15:30:55',
                     message: 'Hai portato a spasso il cane?',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '10/01/2020 15:50:00',
                     message: 'Ricordati di dargli da mangiare',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '10/01/2020 16:15:22',
                     message: 'Tutto fatto!',
-                    status: 'received',
-                    last : true
+                    status: 'received'
+                    
                 }
                 ],
             },
@@ -33,20 +31,17 @@ const app = new Vue({
                 messages: [{
                     date: '20/03/2020 16:30:00',
                     message: 'Ciao come stai?',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '20/03/2020 16:30:55',
                     message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '20/03/2020 16:35:00',
                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received',
-                    last : true
+                    status: 'received'
                 }
                 ],
             },
@@ -57,20 +52,17 @@ const app = new Vue({
                 messages: [{
                     date: '28/03/2020 10:10:40',
                     message: 'La Marianna va in campagna',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '28/03/2020 10:20:10',
                     message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '28/03/2020 16:15:22',
                     message: 'Ah scusa!',
-                    status: 'received',
-                    last : true
+                    status: 'received'
                 }
                 ],
             },
@@ -81,14 +73,12 @@ const app = new Vue({
                 messages: [{
                     date: '10/01/2020 15:30:55',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '10/01/2020 15:50:00',
                     message: 'Si, ma preferirei andare al cinema',
-                    status: 'received',
-                    last : true
+                    status: 'received'
                 }
                 ],
             },
@@ -99,20 +89,17 @@ const app = new Vue({
                 messages: [{
                     date: '10/10/2021 19:30:55',
                     message: 'Stai ancora lavorando al codice?',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '10/10/2021 19:50:00',
                     message: 'Si, sto cercando di risolvere questo problema',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '10/10/2021 19:50:10',
                     message: 'Tu l\'hai risolto?',
-                    status: 'sent',
-                    last : true
+                    status: 'sent'
                 }
                 ],
             },
@@ -123,14 +110,12 @@ const app = new Vue({
                 messages: [{
                     date: '14/07/2021 10:30:55',
                     message: 'Oi tanti auguri di compleanno!!',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '14/07/2021 10:32:00',
                     message: 'Che si fa stasera?',
-                    status: 'received',
-                    last : true
+                    status: 'received'
                 }
                 ],
             },
@@ -141,38 +126,32 @@ const app = new Vue({
                 messages: [{
                     date: '29/11/2021 09:30:55',
                     message: 'Hola, ascolta ci vediamo il pomeriggio?',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '29/11/2021 09:50:00',
                     message: 'Non ci sono, domani va bene lo stesso?',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '29/11/2021 09:55:55',
                     message: 'Va bene, ci vediamo alle 15?',
-                    status: 'sent',
-                    last : false
+                    status: 'sent'
                 },
                 {
                     date: '29/11/2021 10:05:16',
                     message: 'Alle 15 va bene.',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '29/11/2021 10:05:50',
                     message: 'Mi raccomando porta gli appunti',
-                    status: 'received',
-                    last : false
+                    status: 'received'
                 },
                 {
                     date: '29/11/2021 10:30:00',
                     message: 'Assolutamente, a domani allora!',
-                    status: 'sent',
-                    last : true
+                    status: 'sent'
                 },
                 ],
             },
@@ -249,9 +228,54 @@ const app = new Vue({
             
             date = dayjs(date, "DD/MM/YYYY HH:mm:ss").format('HH:mm');
             return date
+        },
+
+        addSetting : function() {
+            this.contacts.forEach( contact => {
+                contact.messages.forEach( message => {
+                    message.setting = false
+                })
+            });
+        },
+
+        // funzione per cambiare la proprietà setting
+        settingsShow : function(message) {
+            console.log(message.setting);
+            message.setting = !message.setting
+            console.log(message.setting);
+            console.log(this.contacts[0].messages[0].setting);
+            
         }
-    }
+    },
+    
+    mounted : function() {
+        this.addSetting()
+    },
 })
+
+// contacts: [
+//     {
+//         name: 'Michele',
+//         avatar: '_1',
+//         visible: true,
+//         messages: [{
+//             date: '10/01/2020 15:30:55',
+//             message: 'Hai portato a spasso il cane?',
+//             status: 'sent'
+//         },
+//         {
+//             date: '10/01/2020 15:50:00',
+//             message: 'Ricordati di dargli da mangiare',
+//             status: 'sent'
+//         },
+//         {
+//             date: '10/01/2020 16:15:22',
+//             message: 'Tutto fatto!',
+//             status: 'received'
+            
+//         }
+//         ],
+//     },
 
 // Milestone 1
 // ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
